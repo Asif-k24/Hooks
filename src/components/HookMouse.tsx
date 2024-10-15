ee9cb39bb3147cb038dc68ff4187746e900cf69d
asif <asif69284@gmail.com>
2024-10-15 17:02:18 +0545

    Clean up code remove event listener

--- src/components/HookMouse.tsx
+++ src/components/HookMouse.tsx
@@ -14,6 +14,11 @@ export default function HookMouse() {
   useEffect(() => {
     console.log('useEffect called');
     window.addEventListener('mousemove', logMousePosition);
+
+    return () => {
+      console.log('Component unmounting code');
+      window.removeEventListener('mousemove', logMousePosition);
+    }
   }, [])
 
   return (
