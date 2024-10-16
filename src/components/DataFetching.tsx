8c0264db30a5ddf65cbd52296f3a1be0111dec7b
asif <asif69284@gmail.com>
2024-10-16 16:54:29 +0545

    Added Interface and button click handle change

--- src/components/DataFetching.tsx
+++ src/components/DataFetching.tsx
@@ -2,53 +2,62 @@ import axios from 'axios';
 import { useState, useEffect } from 'react'
 
 interface postType {
-    userId: number;
-    id: number;
-    title: string;
-    body: string;
+    userId?: number;
+    id?: number;
+    title?: string;
+    body?: string;
 }
 
 export default function DataFetching() {
 
-    const [posts, setPosts] = useState<postType[]>([])
+    const [post, setPost] = useState<postType>({})
+    const [id, setId] = useState(1)
+    const [idFromButtonClick, setIdFromButtonClick] = useState(1)
+
+    const handleClick = () => {
+        setIdFromButtonClick(id);
+    }
 
     // using axios
     useEffect(() => {
-        axios.get("https://jsonplaceholder.typicode.com/posts")
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
             .then(res => {
                 console.log(res);
-                setPosts(res.data)
+                setPost(res.data);
             })
             .catch(err => {
                 console.log(err);
             })
-    }, [])
+    }, [idFromButtonClick])
 
-    /* // using fetch
-        useEffect(() => {
-            fetch("https://jsonplaceholder.typicode.com/posts")
-                .then(res => {
-                    console.log(res);
-                    return res.json();
-                })
-                .then(data => {
-                    console.log(data);
-                    setPosts(data);
-                })
-                .catch(err => {
-                    console.log(err);
-                })
-        }, []) */
+    // using fetch
+    // useEffect(() => {
+    //     fetch("https://jsonplaceholder.typicode.com/posts")
+    //         .then(res => {
+    //             console.log(res);
+    //             return res.json();
+    //         })
+    //         .then(data => {
+    //             console.log(data);
+    //             setPosts(data);
+    //         })
+    //         .catch(err => {
+    //             console.log(err);
+    //         })
+    // }, [])
 
     return (
         <div>
-            <ul>
+            <input type="text" value={id} onChange={(e: any) => setId(e.target.value)} />
+            <button type='button' onClick={handleClick}>Fetch Post</button>
+            <div>{post.title}</ div>
+            {/* <ul>
                 {
-                    posts.map((post: postType) => (
+                    post.map((post: postType) => (
                         <li key={post.id}>{post.title}</li>
                     ))
                 }
-            </ul>
+            </ul> */}
         </div>
     )
 }
